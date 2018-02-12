736fd374535b14dc51e2fa65bd5e53c8376bc7a2
Steven Watts <steven31415@gmail.com>
2018-02-12 15:50:20 +0000

    made test runs show more info

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+
 start_measuring_time() {
   read s1 s2 < <(date +'%s %N')
 }
@@ -7,9 +9,9 @@ stop_measuring_time() {
   read e1 e2 < <(date +'%s %N')
 }
 
-show_elapsed_time() {
-  s=$(bc <<< "scale=3; $((e1-s1))+($((10#$e2-10#$s2))/1000000000)")
-  echo "$s seconds"
+get_elapsed_time() {
+  TIME_TAKEN=$(bc <<< "scale=3; $((e1-s1))+($((10#$e2-10#$s2))/1000000000)")
+  result=$TIME_TAKEN
 }
 
 if [ $# -ne 1 ]; then 
@@ -17,52 +19,89 @@ if [ $# -ne 1 ]; then
   exit -1
 fi
 
-filename="$1"
-test_count=1
-while read -r line
+FILENAME="$1"
+TEST_COUNT=1
+while read -r LINE
 do
-  name="$line"
-
-  if [[ $line == "f_emboss "* ]]; then
-	f_emboss=${line:9}
+  if [[ $LINE == "f_emboss "* ]]; then
+	f_emboss=${LINE:9}
   fi
 
-  if [[ $line == "f_IUPACpal "* ]]; then
-	f_IUPACpal=${line:11}
+  if [[ $LINE == "f_IUPACpal "* ]]; then
+	f_IUPACpal=${LINE:11}
   fi
 
-  if [[ $line == "m "* ]]; then
-	m=${line:1}
+  if [[ $LINE == "m "* ]]; then
+	m=${LINE:1}
   fi
 
-  if [[ $line == "M "* ]]; then
-	M=${line:1}
+  if [[ $LINE == "M "* ]]; then
+	M=${LINE:1}
   fi
 
-  if [[ $line == "g "* ]]; then
-	g=${line:1}
+  if [[ $LINE == "g "* ]]; then
+	g=${LINE:1}
   fi
 
-  if [[ $line == "x "* ]]; then
-	x=${line:1}
+  if [[ $LINE == "x "* ]]; then
+	x=${LINE:1}
   fi
 
-  if [[ $line == "S "* ]]; then
-	S=${line:1}
-  fi
+  if [[ $LINE == "" ]]; then
+	echo "Running test $TEST_COUNT"
 
-  if [[ $line == "" ]]; then
-	echo "Running test $test_count:"
+
 	start_measuring_time
-	./IUPACpal -f $f_IUPACpal -m $m -M $M -g $g -x $x -S $S
+	./IUPACpal -f $f_IUPACpal -m $m -M $M -g $g -x $x &> /dev/null
 	stop_measuring_time
-	show_elapsed_time
-	echo "Test $test_count complete"
-	echo ""
-	echo ""
-	test_count=$((test_count+1))
+	get_elapsed_time
+  IUPACPAL_TIME_TAKEN=$result
+  echo "IUPACpal: $IUPACPAL_TIME_TAKEN seconds"
+
+
+  start_measuring_time
+  ./emboss_palindrome $f_emboss -minpallen $m -maxpallen $M -gaplimit $g -nummismatches $x -outfile emboss_palindrome.out -overlap &> /dev/null
+  stop_measuring_time
+  get_elapsed_time
+  EMBOSS_TIME_TAKEN=$result
+  echo "emboss:   $EMBOSS_TIME_TAKEN seconds"
+	
+
+  python tools/compare_output.py IUPACpal.out emboss_palindrome.out > compare.out
+  TOTAL_IN_IUPACPAL=$(awk '{print $2}' compare.out | sed -n '4p')
+  TOTAL_IN_EMBOSS=$(awk '{print $2}' compare.out | sed -n '5p')
+  IN_BOTH=$(awk '{print $2}' compare.out | sed -n '7p')
+  ONLY_IN_IUPACPAL=$(awk '{print $2}' compare.out | sed -n '8p')
+  ONLY_IN_EMBOSS=$(awk '{print $2}' compare.out | sed -n '9p')
+
+  echo "Pals in IUPACpal: $TOTAL_IN_IUPACPAL"
+  echo "Pals in emboss:   $TOTAL_IN_EMBOSS"
+  echo "Pals in both:     $IN_BOTH"
+  echo "Only in IUPACpal: $ONLY_IN_IUPACPAL"
+  echo "Only in emboss:   $ONLY_IN_EMBOSS"
+
+
+  python tools/check_correctness.py $f_IUPACpal IUPACpal.out $x > correctness.out
+  IUPACPAL_CORRECT_PALS=$(awk '{print $3}' correctness.out | sed -n '6p')
+  IUPACPAL_INCORRECT_PALS=$(awk '{print $3}' correctness.out | sed -n '7p')
+  echo "Correct in IUPACpal:   $IUPACPAL_CORRECT_PALS"
+  echo "Incorrect in IUPACpal: $IUPACPAL_INCORRECT_PALS"
+
+  #this correctness check assumes that f_IUPACpal and f_emboss represent that same data (albeit in different formats)
+  python tools/check_correctness.py $f_IUPACpal emboss_palindrome.out $x > correctness.out
+  EMBOSS_CORRECT_PALS=$(awk '{print $3}' correctness.out | sed -n '6p')
+  EMBOSS_INCORRECT_PALS=$(awk '{print $3}' correctness.out | sed -n '7p')
+  echo "Correct in emboss:   $EMBOSS_CORRECT_PALS"
+  echo "Incorrect in emboss: $EMBOSS_INCORRECT_PALS"
+
+
+  echo "Test $TEST_COUNT complete"
+
+  echo
+	echo
+	TEST_COUNT=$((TEST_COUNT+1))
   fi
-done < "$filename"
+done < "$FILENAME"
 
 echo "All tests completed!"
 
